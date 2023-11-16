7a8bbb72112c314208fe2163be1be369baaa3875
Aep <86505664+Aeplexi@users.noreply.github.com>
2023-11-15 22:01:51 -0500

    r

--- services/Roblox/Roblox.Website/Controllers/Internal/WebController.cs
+++ services/Roblox/Roblox.Website/Controllers/Internal/WebController.cs
@@ -458,11 +458,12 @@ public class WebController : ControllerBase
         if (placeInfo.assetType != Models.Assets.Type.Place) throw new BadRequestException();
         var modInfo = (await services.assets.MultiGetAssetDeveloperDetails(new[] {placeId})).First();
         if (modInfo.moderationStatus != ModerationStatus.ReviewApproved) throw new BadRequestException();
+        var bootstrapperArgs = $"es-player://1+launchmode:play+gameinfo:${Request.Cookies[".ROBLOSECURITY"]}+placelauncherurl:{Configuration.BaseUrl}/game/PlaceLauncher.ashx?placeId={placeId}+k:l";
         var args =
             $"--authenticationUrl {Roblox.Configuration.BaseUrl}/Login/Negotiate.ashx --authenticationTicket {Request.Cookies[".ROBLOSECURITY"]} --joinScriptUrl {Configuration.BaseUrl}/game/placelauncher.ashx?placeId={placeId}";
         return new
         {
-            joinScriptUrl = $"-a http://economysimulator.com/Login/Negotiate.ashx -t {Request.Cookies[".ROBLOSECURITY"]} -j http://economysimulator.com/game/PlaceLauncher.ashx?placeId={placeId}",
+            joinScriptUrl = bootstrapperArgs,
             retroArgs = args
         };
     }
